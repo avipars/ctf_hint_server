5b098ee2b9e1c8ecf9aa68c9ea36e5a9bcc9f1de
avipars <avipars@users.noreply.github.com>
2024-08-14 19:53:14 +0000

    Auto code format

--- flags.py
+++ flags.py
@@ -1,26 +1,21 @@
-stages = { # Sample dictionary with flags and multiple hints 
+stages = {  # Sample dictionary with flags and multiple hints
     1: {
         "flag": "flag{stage1}",
         "hints": [
             "This is the least revealing hint for stage 1.",
             "This is a more revealing hint for stage 1.",
-            "This is the most revealing hint for stage 1."
-        ]
+            "This is the most revealing hint for stage 1.",
+        ],
     },
     2: {
         "flag": "flag{stage2}",
         "hints": [
             "This is the least revealing hint for stage 2.",
             "This is a more revealing hint for stage 2.",
-            "This is the most revealing hint for stage 2."
-        ]
+            "This is the most revealing hint for stage 2.",
+        ],
     },
-    3: {
-        "flag": "flag{stage3}",
-        "hints": [
-            "Almost there"
-        ]
-    }
+    3: {"flag": "flag{stage3}", "hints": ["Almost there"]},
 }
 
 # if __name__ == "__main__":
--- hints/__init__.py
+++ hints/__init__.py
@@ -1,5 +1,10 @@
 from flask import Flask
-# from ctf.error_handlers import register_error_handlers  # Adjust the import based on your project structure
+
+from hints import routes
+
+# from ctf.error_handlers import register_error_handlers  # Adjust the
+# import based on your project structure
+
 
 def create_app():
     app = Flask(
@@ -9,19 +14,24 @@ def create_app():
         template_folder="templates",
     )
     app.config["SECRET_KEY"] = "CTF_K3YF0RS355i0n5!"
-        # ensure session cookie is httponly
+    # ensure session cookie is httponly
     app.config["SESSION_COOKIE_HTTPONLY"] = True
     # ensure session cookie is same site
     app.config["SESSION_COOKIE_SAMESITE"] = "Strict"
-    app.config["REMEMBER_COOKIE_SECURE"] = True  # ensure remember cookie is secure
-    app.config["SESSION_COOKIE_SECURE"] = True  # ensure session cookie is secure
+    # ensure remember cookie is secure
+    app.config["REMEMBER_COOKIE_SECURE"] = True
+    # ensure session cookie is secure
+    app.config["SESSION_COOKIE_SECURE"] = True
 
     with app.app_context():
         from . import routes
+
         app.register_blueprint(routes.bp)
     # register_error_handlers(app)
     # app.config['referrer_policy'] = 'strict-origin-when-cross-origin'
     return app
+
+
 # app = Flask(
 #     __name__,
 #     static_folder="static",
@@ -32,5 +42,3 @@ def create_app():
 
 # register_error_handlers(app)
 # app.config['referrer_policy'] = 'strict-origin-when-cross-origin'
-
-from hints import routes
